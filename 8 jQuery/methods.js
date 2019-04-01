dae777eee886b7d52d64b465e5d413339722f7ed
Nathan-Payne <paynee89@yahoo.co.uk>
2019-04-01 13:13:54 +0800

    add .on() .keypress() and basic JQ effects notes

--- 8 jQuery/methods.js
+++ 8 jQuery/methods.js
@@ -7,7 +7,7 @@ $("h1").text();
 $("ul").html();
 
 //-- .attr() get or set value of attribute of selected element (first if many)
-$("input").attr("type", "color"); //changes input type to be color
+$("#input1").attr("type", "color"); //changes input type to be color
 
 //-- get or set current value of selected element (or first of multiple)
 $("input").val();
@@ -19,3 +19,32 @@ $("h1").removeClass("correct"); //removes class correct from h1
 $("li").last().toggleClass("done");  //toggles done for last() li
 $("ul li").toggleClass("done"); //toggles done for all li - adds to li1 li2 removes from li3
 
+//-- events in jquery
+//-- click() adds click listener to slected element
+$("#thing").click(function(){
+    console.log("thing to do..");
+});
+//-- if multiple selected $("button") then function activated when any are clicked
+$("button").click(function(){
+    $(this).css("background-color", "darkblue");
+}); //-- $(this) is jquery's version of js this - works on individual button from list
+
+//-- keypress() binds event handler to keypressed (value of key after keydown before keyup)
+$("input").keypress(function(event){
+    console.log(event);
+}) //-- logs all info regarding keypress in console including char code/which
+
+$("input").keypress(function(event){
+    if(event.which===13){
+        alert("13 is keycode for enter");
+    };
+});
+
+//TESTING STUFF
+$("#checkbox").click(function(){
+    $("#input1").attr("type", "text");
+    $("#input1").val("");
+    $("body").css("background-color", "grey");
+});
+
+//-- on() similiar to addEventListener(). must specify type of event to use
