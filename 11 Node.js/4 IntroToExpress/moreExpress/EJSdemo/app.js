7b0f9aebd6ffe9afad2af0423794e74bd46d75ae
Nathan-Payne <paynee89@yahoo.co.uk>
2019-04-06 22:55:58 +0800

    add partials folder in /views -provide template for head/footer

--- 11 Node.js/4 IntroToExpress/moreExpress/EJSdemo/app.js
+++ 11 Node.js/4 IntroToExpress/moreExpress/EJSdemo/app.js
@@ -2,6 +2,10 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public")); //tells express to serve contents of public directory (where .css found)
+//app.set("view engine", "ejs"); //tells express will be using .ejs files ahead of time so dont need to
+                                    //type .ejs file extension every time
+
 //res.render(name of html-ejs file) --express will look for "views" folder to find pages to render
 //ejs=embbedded java script:  --need to npm install
                             //--uses <%= JAVASCRIPT HERE %> to include JS in ejs file
@@ -13,6 +17,16 @@ app.get("/", function(req, res){
 app.get("/skyrim/:Race", function(req, res){
     var skyrimRace = req.params.Race;
     res.render("skyrim.ejs", {skyrimRaceEJS: skyrimRace});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "First", author: "Bear"}, 
+        {title: "Second!", author: "Floo"}, 
+        {title: "Whatever guys", author: "Davina"}
+    ];
+
+    res.render("posts.ejs", {posts: posts})
 });
 
 app.listen(3000, function(){
